1f839488a6764c9cbec3861e01524ce8569f93dd
ROMANOZZI <mohamedsalah2162000@gmail.com>
2023-08-31 16:46:38 +0300

    secondcommit

--- Helloworld.js
+++ Helloworld.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
+  console.log("hello world");
+  debugger;
   res.send("hello World");
 });
 app.listen(port, () => {
